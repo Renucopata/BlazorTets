f053edb29e0e906821a36c7484dede6cf629ed88
Robotltd Lab. Transformacion digital <robotltd@adecofuturo.bo>
2023-05-29 18:16:53 -0400

    login connection

---
+++ BlazorApp1/Models/AuthenticatedUserModel.cs
@@ -0,0 +1,13 @@
+﻿namespace BlazorApp1.Models
+{
+	public class AuthenticatedUserModel
+	{
+		public string UserName { get; set;}
+		public string Password { get; set;}
+		public string Email { get; set;}
+		public string Nombre { get; set;}
+		public string rol { get; set;}
+		public int idAgenda { get; set;}
+		
+	}
+}
---
+++ BlazorApp1/Models/AuthenticationUserModel.cs
@@ -0,0 +1,16 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+
+namespace BlazorApp1.Models
+
+{
+	public class AuthenticationUserModel
+	{
+		[Required]
+		public string UserName { get; set; }
+
+		[Required]
+		public string Password { get; set; }
+	}
+}
