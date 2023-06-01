46b3d47826a344b0ef441d8fa65e96ca2e8081a1
Robotltd Lab. Transformacion digital <robotltd@adecofuturo.bo>
2023-06-01 18:13:01 -0400

    saul authentication

--- BlazorApp1/Models/ApiResponseModel.cs
+++ BlazorApp1/Models/ApiResponseModel.cs
@@ -1,15 +1,23 @@
-﻿namespace BlazorApp1.Models
+﻿using System.Text.Json.Serialization;
+
+namespace BlazorApp1.Models
 {
 	public class ApiResponseModel
 	{
 
 		//Change the  JsonPropertyName to the exact headers name
 
+		[JsonPropertyName("login")]
 		public string UserName { get; set;}
-		public string Password { get; set;}
+
+		[JsonPropertyName("email")]
 		public string Email { get; set;}
-		public string Nombre { get; set;}
+	
+
+		[JsonPropertyName("cargo")]
 		public string Cargo { get; set;}
+
+		[JsonPropertyName("codigo_agenda")]
 		public int IdAgenda { get; set;}
 		
 	}
--- BlazorApp1/Models/tokenRequest.cs
+++ BlazorApp1/Models/tokenRequest.cs
@@ -3,7 +3,7 @@ using System.Text.Json.Serialization;
 
 namespace BlazorApp1.Models
 {
-	public class tokenRequest
+	public class TokenRequest
 	{
 		[JsonPropertyName("token")]
 		public string Token { get; set; }
